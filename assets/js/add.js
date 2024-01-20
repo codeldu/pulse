0f06982872882978899899d7b76a6fe7c3c12a96
codeldu <huseynagha.hagverdiyev@code.edu.az>
2024-01-20 05:06:28 +0400

    updated

--- assets/js/add.js
+++ assets/js/add.js
@@ -4,25 +4,18 @@ let desc = document.querySelector("#desc");
 let price = document.querySelector("#price");
 let table = document.querySelector("table");
 let modal = document.querySelector(".modal");
-let closeBtn = document.querySelector("#closeBtn")
-let nameError = document.querySelector("#nameError");
-let descError = document.querySelector("#descError");
-let priceError = document.querySelector("#priceError");
+let closeBtn = document.querySelector("#closeBtn");
+
+// let nameError = document.querySelector("#nameError");
+// let descError = document.querySelector("#descError");
+// let priceError = document.querySelector("#priceError");
 
 form.addEventListener("submit", (event)=> {
     event.preventDefault();
      
-    const errorTexts = [nameError, descError, priceError]
+    const inputs = [name, desc, price];
 
-    errorTexts.forEach((text)=> text.style.display = 'none')
-
-    let nameInp = name.value.trim();
-    let descInp = desc.value.trim();
-    let priceInp = price.value.trim();
-
-
-    
-    if(nameInp && descInp && priceInp){
+    if(name.value.trim() && desc.value.trim() && price.value.trim()){
         let obj = {
             name: name.value,
             desc: desc.value,
@@ -32,27 +25,72 @@ form.addEventListener("submit", (event)=> {
         .then(res => {
             window.location = "./index.html"
         })
+    }else{
+        inputs.forEach(input => {
+            
+            // Adi if else ile
+        //    if(input.value.trim() == ""){
+        //     input.previousElementSibling.style.display = 'block'
+        //    }else{
+        //     input.previousElementSibling.style.display = 'none'
+        //    }
+            // ternary operator ile
+        let currentDisplay = input.value.trim() == "" ? 'block' : 'none'
+        input.previousElementSibling.style.display = currentDisplay;
+        })
     }
 
 
-    // ===with modal ===
-    // else{
-    //     modal.style.display = "flex";
-    //     modal.style.position = "fixed";
-    // }
 
-
-    // == tek-tek yoxlama == 
-    else if(!nameInp){
-        nameError.style.display = "block"
-    }
-    else if(!descInp){
-        descError.style.display = "block"
-    }
-    else{
-        priceError.style.display = "block"
-    }
 })
+
+
+
+
+// form.addEventListener("submit", (event)=> {
+//     event.preventDefault();
+     
+//     const errorTexts = [nameError, descError, priceError]
+
+//     errorTexts.forEach((text)=> text.style.display = 'none')
+
+//     let nameInp = name.value.trim();
+//     let descInp = desc.value.trim();
+//     let priceInp = price.value.trim();
+
+
+    
+//     if(nameInp && descInp && priceInp){
+//         let obj = {
+//             name: name.value,
+//             desc: desc.value,
+//             price: price.value
+//         }
+//         axios.post("http://localhost:3000/Pulse", obj)
+//         .then(res => {
+//             window.location = "./index.html"
+//         })
+//     }
+
+
+//     // ===with modal ===
+//     // else{
+//     //     modal.style.display = "flex";
+//     //     modal.style.position = "fixed";
+//     // }
+
+
+//     // == tek-tek yoxlama == 
+//     else if(!nameInp){
+//         nameError.style.display = "block"
+//     }
+//     else if(!descInp){
+//         descError.style.display = "block"
+//     }
+//     else{
+//         priceError.style.display = "block"
+//     }
+// })
 
 closeBtn.addEventListener("click", ()=> {
     modal.style.display = "";
